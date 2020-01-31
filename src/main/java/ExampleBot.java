2bbf92cd58bc6738a6d74eb23dfd8897a291d95f
smichale <smichale@nd.edu>
2020-01-31 14:23:05 -0500

    Initial commit

---
+++ src/main/java/ExampleBot.java
@@ -0,0 +1,58 @@
+import bwapi.*;
+
+public class ExampleBot extends DefaultBWListener {
+    BWClient bwClient;
+    Game game;
+
+    @Override
+    public void onStart() {
+        game = bwClient.getGame();
+    }
+
+    @Override
+    public void onFrame() {
+        Player self = game.self();
+        game.drawTextScreen(100, 100, "Bot has " + self.minerals() + " minerals");
+        game.drawTextScreen(100, 120, "Bot has " + self.allUnitCount() + " units");
+
+        // Train units while we can
+        for (Unit trainer : self.getUnits()) {
+            UnitType unitType = trainer.getType();
+            if (unitType.isBuilding() && !unitType.buildsWhat().isEmpty()) {
+                UnitType toTrain = unitType.buildsWhat().get(0);
+                if (game.canMake(toTrain, trainer)) {
+                    trainer.train(toTrain);
+                    // specific commands for training specific unit examples:
+                    // commandCenter.train(UnitType.Terran_SCV);
+                    // larva.morph(UnitType.Zerg_Drone);
+                    // larva.morph(UnitType.Zerg_Overlord);
+                }
+            }
+        }
+    }
+
+    public void onUnitComplete(Unit unit) {
+        if (unit.getType().isWorker()) {
+            Unit closestMineral = null;
+            int closestDistance = Integer.MAX_VALUE;
+            for (Unit mineral : game.getMinerals()) {
+                int distance = unit.getDistance(mineral);
+                if (distance < closestDistance) {
+                    closestMineral = mineral;
+                    closestDistance = distance;
+                }
+            }
+            // Gather the closest mineral
+            unit.gather(closestMineral);
+        }
+    }
+
+    void run() {
+        bwClient = new BWClient(this);
+        bwClient.startGame();
+    }
+
+    public static void main(String[] args) {
+        new ExampleBot().run();
+    }
+}
